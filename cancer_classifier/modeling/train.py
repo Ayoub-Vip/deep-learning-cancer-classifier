7161725c2d431f58034058f8ed8ce9fe1487637b
ayoub-vip <ayoub.vip1978@gmail.com>
2025-05-20 17:13:25 +0200

    Add all models

--- cancer_classifier/modeling/train.py
+++ cancer_classifier/modeling/train.py
@@ -4,6 +4,7 @@ Example:
 """
 
 import os
+import sys
 import time
 import torch
 import numpy as np
@@ -23,6 +24,7 @@ def get_model(model_name, model_class, args, device):
     if model_name == "cnn":
         model = model_class(
             fc_dropout_prob=args['fc_dropout_prob'],
+            dropout_prob=args['dropout_prob'],
             num_classes=3
         )
     elif model_name == "vit":
@@ -64,51 +66,78 @@ def train(config, model_class, device, dataset, checkpoint_dir=None):
         model.load_state_dict(checkpoint["model_state_dict"])
         optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
 
-    train_losses, val_losses = [], []
-    train_accuracies, val_accuracies = [], []
+    train_losses, val_losses, test_losses = [], [], []
+    train_accuracies, val_accuracies, test_accuracies = [], [], []
+    true_labels, pred_labels = [], []
 
     for epoch in tqdm(range(config["epochs"]), desc="Training"):
         model.train()
-        for images, labels in train_loader:
-            images, labels = images.to(device), labels.to(device)
+        for X, y in train_loader:
+            X, y = X.to(device), y.to(device)
 
             optimizer.zero_grad()
-            outputs = model(images)
-            loss = criterion(outputs, labels)
+            pred = model(X)
+            loss = criterion(pred, y)
             loss.backward()
             optimizer.step()
 
-            acc = (outputs.argmax(dim=1) == labels).float().mean().item()
+            acc = (pred.argmax(dim=1) == y).float().mean().item()
             train_losses.append(loss.item())
             train_accuracies.append(acc)
 
         model.eval()
         with torch.no_grad():
-            for images, labels in val_loader:
-                images, labels = images.to(device), labels.to(device)
+            for X, y in val_loader:
+                X, y = X.to(device), y.to(device)
 
-                outputs = model(images)
-                loss = criterion(outputs, labels)
+                pred = model(X)
+                loss = criterion(pred, y)
 
-                acc = (outputs.argmax(dim=1) == labels).float().mean().item()
+                acc = (pred.argmax(dim=1) == y).float().mean().item()
                 val_losses.append(loss.item())
                 val_accuracies.append(acc)
+        
+        model.eval()
+        with torch.no_grad():
+            for X, y in test_loader:
+                X, y = X.to(device), y.to(device)
+
+                pred = model(X)
+                loss = criterion(pred, y)
+
+                acc = (pred.argmax(dim=1) == y).float().mean().item()
+                test_losses.append(loss.item())
+                test_accuracies.append(acc)
+                true_labels.extend(y.cpu().numpy())
+                pred_labels.extend(pred.argmax(1).cpu().numpy())
 
     avg_train_loss = np.mean(train_losses)
     avg_val_loss = np.mean(val_losses)
+    avg_test_loss = np.mean(test_losses)
+    
     avg_train_acc = np.mean(train_accuracies)
     avg_val_acc = np.mean(val_accuracies)
+    avg_test_acc = np.mean(test_accuracies)
 
     tune.report({
-        "val_accuracy" : avg_val_acc,
         "train_losses" : train_losses,
         "val_losses" : val_losses,
-        "train_accuracies" : train_accuracies,
-        "val_accuracies" : val_accuracies,
+        "test_losses" : test_losses,
+        
         "avg_train_loss" : avg_train_loss,
         "avg_val_loss" : avg_val_loss,
+        "avg_test_loss" : avg_test_loss,
+        
+        "train_accuracies" : train_accuracies,
+        "val_accuracies" : val_accuracies,
+        "test_accuracies" : test_accuracies,
+        
         "avg_train_acc" : avg_train_acc,
-        "avg_val_acc" : avg_val_acc
+        "avg_val_acc" : avg_val_acc,
+        "avg_test_acc" : avg_test_acc,
+        
+        "true_labels" : true_labels,
+        "pred_labels" : pred_labels
         }
     )
 
